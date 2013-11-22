f98971b0cc7e2a5bc389ed8c6cb48722dd63a42c
genemills <genemills@gmail.com>
2013-11-22 10:58:02 -0500

    dom complete

--- dom.js
+++ dom.js
@@ -10,6 +10,15 @@
 // Find the Google search button and store it in a variable.
 // Modify the text of the button so that it says "Yahooo!" instead.
 
+var logo = document.getElementById("hplogo");
+logo.src = "http://l.yimg.com/rz/d/yahoo_frontpage_en-CA_s_f_p_101x50_frontpage.png";
+
+var searchButton = document.getElementById("gbqfsa");
+searchButton.innerHTML = "Yahoo is the new Google!";
+
+
+
+
 // EXERCISE: About Me
 
 // Cuz every webpage needs an "About" section.
